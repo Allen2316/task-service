69b04266575cc2840ef9a9f0dd305a94f3fdf9df
Luis Alberto Sarmiento Cuenca <52260850+Allen2316@users.noreply.github.com>
2023-07-16 16:00:18 -0500

    update

--- src/main/java/com/allen/task/TaskService.java
+++ src/main/java/com/allen/task/TaskService.java
@@ -2,12 +2,13 @@ package com.allen.task;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-public class PortafolioApplication {
+public class TaskService {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PortafolioApplication.class, args);
+		SpringApplication.run(TaskService.class, args);
 	}
 
 }
