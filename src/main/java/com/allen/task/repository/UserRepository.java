d97ba37fded63849bf41ba9e973af3fb2b65eec8
Luis Alberto Sarmiento Cuenca <52260850+Allen2316@users.noreply.github.com>
2023-07-16 04:41:52 -0500

    Update

--- src/main/java/com/allen/task/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.allen.backend.repository;
-
-import com.allen.backend.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    //Todo Hacer el repository
-}
